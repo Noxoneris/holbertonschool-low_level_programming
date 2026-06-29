40e064da4a1f771124c73d8d1f73f40e072aee64
Noxoneris <noxoneris@hotmail.fr>
2026-06-29 10:38:52 +0200

    Add sum_to_n.c

---
+++ intro_debugging/README.md
@@ -0,0 +1,1 @@
+# 0.Debugging a Counting Function
---
+++ intro_debugging/sum_to_n.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * sum_to_n - sums all integers from 1 to n
+ * @n: the integer to sum to
+ *
+ * Return: the sum of all integrers from 1 to n
+ */
+int sum_to_n(int n)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i <= n; i++)
+		sum += i;
+	return sum;
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+	printf("%d\n", sum_to_n(10));
+	return 0;
+}
