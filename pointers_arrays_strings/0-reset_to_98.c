8029bc4e511fd1fb102e765cbff5ab757b30a603
Noxoneris <noxoneris@hotmail.fr>
2026-07-02 22:21:00 +0200

    Add 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an integer value to 98 using a pointer
+ * @n: the memory address of the integer to modify
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Pointers, Arrays and Strings
+
+a function that takes the basics of pointers, arrays and strings in C
