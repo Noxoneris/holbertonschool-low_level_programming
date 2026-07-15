a7d6de1d4bbf852fc1c43c27060b08a50c15bddc
Noxoneris <noxoneris@hotmail.fr>
2026-07-15 11:17:06 +0200

    1. Whoever is calling - array_iterator function

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: the array to iterate over
+ * @size: the size of the array
+ * @action: pointer to the function to use on each element
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
